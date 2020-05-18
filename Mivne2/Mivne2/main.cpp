1d6af5262780251d5faa9753450018f2168e0716
nadavzeldin <52635084+nadavzeldin@users.noreply.github.com>
2020-05-18 20:18:06 +0300

    simple main

---
+++ Mivne2/Mivne2/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int n,k;
+    char firstName[100];
+    char lastName[100];
+    cout << "Please enter number of items: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Please enter id and first name and last name";
+        cin >> n;
+        cin >> firstName;
+        //need to check whitepace between first and last name
+        cin >> lastName;
+    }
+    int k;
+
+    //need to print all items which id is less then k
+        // NaivePrint
+        //BSTPrint
+        //PrintBySort
+
+    return 0;
+}
